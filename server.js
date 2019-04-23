91b5cee3a5c1cafa13dfc24be7831225be485e9b
Irving Duran <ijd.irving@gmail.com>
2019-04-23 18:09:06 -0300

    mvp

--- server.js
+++ server.js
@@ -53,48 +53,63 @@ server.post("/api/posts", (req, res) => {
     });
 });
 
-server.put("/api/posts/:id", (res, req) => {
+server.put("/api/posts/:id", (req, res) => {
   const { id } = req.params;
   const { title, contents } = req.body;
   if (!title || !contents) {
     res.status(400).json({
       error: err,
       message: "Please provide title and contents for the post."
-    }) 
+    });
     return;
   } else {
-    res.status(200)
+    res.status(200);
   }
-  Database
-  .update(id, { title, contents })
-  .then(response => {
+  Database.update(id, { title, contents })
+    .then(response => {
       if (response == 0) {
         res.status(404).json({
-            error: err,
-            message: "The post with the specified ID does not exist."
-          });
-          return;
-      } else {
-        res.status(200)
-      }
-    Database.findById(id)
-      .then(post => {
-        if (post.length === 0) {
-          res.status(404).json({
-            message: "The post with the specified ID does not exist.",
-            res
-          });
-          return;
-        }
-        res.json(post);
-      })
-      .catch(err => {
-        res.status(500).json({
           error: err,
-          message: "The post information could not be modified."
+          message: "The post with the specified ID does not exist."
         });
+        return;
+      } else {
+        res.status(200);
+      }
+      Database.findById(id)
+        .then(post => {
+          if (post.length === 0) {
+            res.status(404).json({
+              message: "The post with the specified ID does not exist.",
+              res
+            });
+            return;
+          }
+          res.json(post);
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err,
+            message: "The post information could not be modified."
+          });
+        });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+        message: "The post information could not be modified."
       });
-  });
+    });
 });
+
+server.delete("/api/:id", (req, res) => {
+    const {id} = req.params;
+    Database.remove(id).then(deleted => {
+        res.status(204).end(deleted);
+    }).catch(err => {
+        res.status(500)
+        .json({ error: err, message: "error"})
+    })
+})
 
 module.exports = server;
